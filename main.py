2af0d40ca32ab7e0d5504116cbe5effb07889fa5
kov-up <dendencout@gmail.com>
2025-05-21 18:06:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ b = int(input('b = '))
 c = int(input('c = '))
 if a > b:
     if a > c:	
-        print('a - maximum')
+        print('a - max')
 elif b > c:	
-    print('b - maximum')
+    print('b - max')
 else:
-    print('c - maximum')
+    print('c - max')
 print('min + max = ', max(a,b,c) + min(a,b,c))
 print(min(a, b, c))
 print(max(a, b, c))
